222069ae93f6a02230172288818a26cd793e3de3
Karl Grzeszczak <karl@karlgrz.com>
2015-09-04 16:47:58 -0500

    trivial persistent storage example

--- src/main.rs
+++ src/main.rs
@@ -1,42 +1,69 @@
 extern crate iron;
 extern crate rustc_serialize;
 extern crate router;
+extern crate persistent;
 
 use iron::prelude::*;
+use iron::typemap::Key;
 use iron::status;
+
+use persistent::Write;
+
 use router::Router;
 use rustc_serialize::json;
 use std::io::Read;
 
+#[derive(Copy, Clone)]
+pub struct JobStore;
+
+impl Key for JobStore { type Value = usize; }
+
+#[derive(Debug)]
 #[derive(RustcDecodable, RustcEncodable)]
 struct Job {
     id: String,
-    msg: String
+    msg: String,
+    count: usize
 }
 
 fn status(request: &mut Request) -> IronResult<Response> {
+    let mutex = request.get::<Write<JobStore>>().unwrap();
+    let mut count = mutex.lock().unwrap();
+
+    *count += 1;
+
     let ref id = request.extensions.get::<Router>().unwrap().find("id").unwrap_or("/");
-    let job = Job { id: id.to_string(), msg : "test".to_string() };
+    let job = Job { id: id.to_string(), msg : "test".to_string(), count: *count };
     let payload = json::encode(&job).unwrap();
     Ok(Response::with((status::Ok, payload)))
 }
 
 fn set_job(request: &mut Request) -> IronResult<Response> {
+    let mutex = request.get::<Write<JobStore>>().unwrap();
+    let mut count = mutex.lock().unwrap();
+
     let mut payload = String::new();
 
     request.body.read_to_string(&mut payload).unwrap();
 
     let request: Job = json::decode(&payload).unwrap();
-    let job = Job { id: request.id, msg: request.msg };
+
+    *count += request.count;
+
+    let job = Job { id: request.id, msg: request.msg, count: *count };
     let payload = json::encode(&job).unwrap();
     Ok(Response::with((status::Ok, payload)))
 }
 
 fn main() {
+
     let mut router = Router::new();
 
     router.get("/status/:id", status);
     router.post("/job", set_job);
 
-    Iron::new(router).http("localhost:3000").unwrap();
+    let mut chain = Chain::new(router);
+    chain.link(Write::<JobStore>::both(0));
+
+    Iron::new(chain).http("localhost:3000").unwrap();
 }
